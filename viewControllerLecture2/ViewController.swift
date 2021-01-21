59b4951c12367a4e0f4ba49ea6685a868e022efd
romanthegg <rzadorskyy3454@stu.d214.org>
2021-01-21 23:13:42 +0200

    Initial Commit

---
+++ viewControllerLecture2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  viewControllerLecture2
+//
+//  Created by Yanik Kukulnik on 21.01.2021.
+//  Copyright © 2021 Yanik Kukulnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
